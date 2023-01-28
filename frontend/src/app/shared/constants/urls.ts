c15eeafeae7e01ec8f83499c420a2cfa377380a6
unknown <rimouarrak2002@gmail.com>
2023-01-28 16:29:30 +0100

    login & logout

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,4 +5,5 @@ export const GEMS_TAGS = GEMS_URL + '/tags';
 export const GEMS_BY_SEARCH_URL = GEMS_URL + '/search/';
 export const GEMS_BY_TAG_URL = GEMS_URL + '/tag/';
 export const GEMS_BY_ID_URL = GEMS_URL + '/';
- 
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
